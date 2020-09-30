9d7053c40c1b274dbf9078f485788dea81e79232
Nazanin-hub <n.tvkl1989@gmail.com>
2020-09-30 08:51:40 -0700

    Adding the election analysis.

--- Python_practice.py
+++ Python_practice.py
@@ -14,3 +14,33 @@ for county in counties:
 counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
 for county, voters in counties_dict.items():
     print(f"{county} has {voters} registered voters")
+
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# 1. Initialize a total vote counter.
+total_votes_C = 0
+total_votes_D = 0
+total_votes_R = 0
+# Candidate options and candidate votes
+#candidate_options = []
+# 1. Declare the empty dictionary.
+#candidate_votes = {}
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read the header row.
+    headers = next(file_reader)
+for row in file_reader:
+     if candidate_name = "Charles Casper Stockham":
+         total_votes_C += 1
+     elif candidate_name = "Diana DeGette":
+         total_votes_D += 1 
+     else candidate_name = "Raymon Anthony Doane":
+         total_votes_R += 1 
+print(total_votes_c,total_votes_D,total_votes_R)
