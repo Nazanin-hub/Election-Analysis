e738419bc448abcfd5df7a6eda03d5ffe957a859
Nazanin-hub <n.tvkl1989@gmail.com>
2020-10-09 12:34:27 -0700

    Adding the election analysis

--- Python_practice.py
+++ Python_practice.py
@@ -44,3 +44,39 @@ for row in file_reader:
      else candidate_name = "Raymon Anthony Doane":
          total_votes_R += 1 
 print(total_votes_c,total_votes_D,total_votes_R)
+
+name =" Joe"
+country = " USA"
+age = 35
+hourly_wage = 70
+satisfied = True
+daily_wage = hourly_wage*8
+print(f"hello {name}")
+print(f" I live in {country}")
+print(f" I earn {daily_wage}")
+print(f" are you satisfied with your salary?{satisfied}")
+
+List= ['Milk','Bread','Eggs','Peanut Butter','Jelly' ]
+print(List)
+List[3]= 'Almond Butter'
+List.remove('Jelly')
+List.append('coffee')
+print(List)
+
+
+Nazanin_Dictionary = { 'name': "Nazanin", 'age':"31" ,'hobbies':["running" ,"playing Piano"]
+Nazanin_Dictionary2 = {'times':["9.00","8:00"]}
+
+print(Nazanin_Dictionary)
+
+
+
+
+
+
+
+
+
+
+
+
